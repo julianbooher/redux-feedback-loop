afa588cf002f2536f9d29adedfba271486d84064
julianbooher <julianbooher@hotmail.com>
2020-12-12 09:55:25 -0600

    tried to make a non-specific funtion to handle submission and action type on the redux end

--- src/components/Feel/Feel.js
+++ src/components/Feel/Feel.js
@@ -11,25 +11,44 @@ class Feel extends Component {
     }
 
     handleSubmit = () => {
+        let stateKey = Object.keys(this.state)[0]
         console.log('in submit');
-        
+        console.log(this.state[stateKey])
+        // TO DO input validation with sweetalert? if/else
+        this.props.dispatch({type: 'UPDATE_VALUE', payload: {
+            key: stateKey,
+            value: this.state[stateKey]
+        }})
+    }
+
+    componentDidMount(){
+        this.setState({
+            feeling: this.props.reduxState.feeling
+        })
     }
 
     handleChangeFor = (event, input) => {
         this.setState({
-            newCustomer: {
-                ...this.state.newCustomer,
-                [input]: event.target.value
-            },
-        });
+                [input]: Number(event.target.value)
+            })
     }
 
     render() {
         return (
             <div>
+                {JSON.stringify(this.state)}
+                <br>
+                </br>
+                {JSON.stringify(this.props.reduxState)}
                 <h1>How are you feeling today?</h1>
                 <form onSubmit={this.handleSubmit}>
-                    <input  required min="1" max="10" type="number" placeholder="1-10" value={this.state.value} onChange={this.handleChangeFor} />
+                    <input  
+                        required min="1" 
+                        max="10" 
+                        type="number" 
+                        placeholder="1-10" 
+                        value={this.state.feeling} 
+                        onChange={(event) => this.handleChangeFor(event, 'feeling')} />
                     <button type="submit">Next</button>
                 </form>
             </div>
--- src/index.js
+++ src/index.js
@@ -11,10 +11,28 @@ import {Provider} from 'react-redux';
 import logger from 'redux-logger';
 
 
-const feedbackReducer = (state = {}, action) => {
+const feedbackReducer = (
+    state = {
+        feeling: 1,
+        understanding: 1,
+        support: 1,
+        comment: ''
+    }, 
+    action) => {
+    // TODO set initial state to default initial values, and then add component did mount for each component and set state to feedback reducer. The user can go back and it will retain the previous value that the form had. 
     //TODO action type
     // state is an object, so we're just gonna update one key:value pair at a time using the payload.
     // payload will contain a key and a value.
+
+    if(action.type === 'UPDATE_VALUE'){
+        console.log(action.payload);
+        let returnState = {
+            ...state,
+            [action.payload.key]: action.payload.value
+        }
+        console.log(returnState);
+        return returnState
+    }
     return state;
 }
 
